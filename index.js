0abdd54b9e24292b5f32a812c0300695a07d4afe
Bin Ury <1146921+teddy-error@users.noreply.github.com>
2018-11-01 17:18:57 -0500

    Fix default notification title to resolve parent's name rather than self

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const fs = require('fs');
+
 class Notifier {
   constructor(options) {
     this.options = options;
@@ -7,9 +9,10 @@ class Notifier {
     compiler.plugin('done', (stats) => {
       const notifier = require('node-notifier');
       const time = ((stats.endTime - stats.startTime) / 1000).toFixed(2);
+      const packageJsonPath = path.join('..', '..', 'package.json');
 
       notifier.notify({
-        title: this.options.title || require(path.join(__dirname, 'package.json')).name,
+        title: this.options.title || fs.existsSync(packageJsonPath) ? require(packageJsonPath).name : 'build-notifier',
         message: `Build completed!\n${stats.compilation.errors.length} errors in ${time}s`,
         contentImage: this.options.logo || ''
       });
